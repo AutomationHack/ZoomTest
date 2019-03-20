13fad97a5afd6ebcb91efa571a65e9d812abd9a8
Anandcmsd7 <48678965+Anandcmsd7@users.noreply.github.com>
2019-03-20 16:21:02 +0000

    test

---
+++ src/webparts/migrationTest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMigrationTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MigrationTestWebPartStrings' {
+  const strings: IMigrationTestWebPartStrings;
+  export = strings;
+}
